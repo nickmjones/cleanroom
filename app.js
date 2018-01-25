191fa3a4175bb74658f2eb8fae698c92585621b9
Nick Jones <hello@nickmjones.me>
2018-01-25 16:18:03 -0500

    Fixes and additions: - adds route for future examples - fixes to app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var example = require('./routes/example');
 
 var app = express();
 
@@ -30,7 +30,7 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
-app.use('/users', users);
+app.use('/example', example)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -49,7 +49,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-app.use(require("better-express-errors")(app))
 
 module.exports = app;
--- routes/example.js
+++ routes/example.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET examples page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Home | Cleanroom' });
+  res.render('example', { title: 'Examples | Cleanroom' });
 });
 
 module.exports = router;
